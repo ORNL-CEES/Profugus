a24dbddfcb5409d5eb7016f0f1d269e946d38f2e
Tom Evans <evanstm@ornl.gov>
2014-03-31 10:23:33 -0400

    Updated x86-64 scripts.

--- install/cmake_x86_64.sh
+++ install/cmake_x86_64.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 ##---------------------------------------------------------------------------##
-## CMAKE FOR MACOSX
+## CMAKE FOR X86_64
 ##---------------------------------------------------------------------------##
 
-# >>>>> EDIT PATHS HERE <<<<<
-
+# SOURCE AND INSTALL
 SOURCE=<SET_SOURCE_DIR>
 INSTALL=<SET_INSTALL_DIR>
+
+# BUILD OPTIONS
 BUILD="DEBUG"
 MPI="ON"
 
-HDF5_PATH="/vendors/parallel/hdf5"
+# TPL PATHS
+HDF5_PATH="/vendors/hdf5_parallel"
+MPI_PATH="/opt/openmpi/gcc/current"
 
 ##---------------------------------------------------------------------------##
 
@@ -20,13 +23,19 @@ cmake \
 -DCMAKE_INSTALL_PREFIX:PATH=$INSTALL \
 -DProfugus_CONFIGURE_OPTIONS_FILE:FILEPATH="${SOURCE}/install/base.cmake" \
 \
--DMPI_BASE_DIR:PATH=/opt/mpi/current \
+-DMPI_BASE_DIR:PATH=$MPI_PATH \
 \
 -DTPL_ENABLE_HDF5:BOOL=ON \
 -DHDF5_INCLUDE_DIRS:PATH=$HDF5_PATH/include \
 -DHDF5_LIBRARY_DIRS:PATH=$HDF5_PATH/lib \
+\
+-DBLAS_LIBRARY_DIRS:PATH=/vendors/gcc/atlas/lib \
+-DLAPACK_LIBRARY_DIRS:PATH=/vendors/gcc/atlas/lib \
+-DBLAS_LIBRARY_NAMES:STRING="f77blas;cblas;atlas" \
+-DLAPACK_LIBRARY_NAMES:STRING="lapack" \
+\
 ${SOURCE}
 
 ##---------------------------------------------------------------------------##
-## end of cmake_macosx.sh
+## end of cmake_x86_64.sh
 ##---------------------------------------------------------------------------##
