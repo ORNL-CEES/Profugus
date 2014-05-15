f8a3bba4b6864b36dc358cc3ae9eb8498f4e5727
Tom Evans <evanstm@ornl.gov>
2014-05-15 17:19:44 -0400

    Added keff-tally tests.

--- packages/MC/mc/test/tstKeff_Tally.cc
+++ packages/MC/mc/test/tstKeff_Tally.cc
@@ -108,8 +108,6 @@ class Keff_TallyTest : public testing::Test
     RCP_Std_DB  db;
     SP_Physics  physics;
 
-    Particle_t p;
-
     int node;
     int nodes;
 };
@@ -122,6 +120,9 @@ TEST_F(Keff_TallyTest, test_keff)
 {
     // Make a keff tally
     Tally_t keff(1.0, physics);
+
+    // make a particle
+    Particle_t p;
 
     // Set reference k
     double ref_k = 0.0;
@@ -160,7 +161,245 @@ TEST_F(Keff_TallyTest, test_keff)
     keff.end_cycle(3.0);
 
     EXPECT_SOFTEQ(nodes * ref_k / 3.0, keff.latest(), 1.0e-6);
-    
+
+    // >>> TEST THE RESET METHOD
+
+    keff.reset();
+
+    // latest keff should stay the same
+    EXPECT_SOFTEQ(nodes * ref_k / 3.0, keff.latest(), 1.0e-6);
+
+    // path lengths and counter should be reset
+    EXPECT_EQ(0., keff.keff_sum());
+    EXPECT_EQ(0., keff.keff_sum_sq());
+    EXPECT_EQ(0, keff.cycle_count());
+}
+
+//---------------------------------------------------------------------------//
+
+TEST_F(Keff_TallyTest, test_keff_var)
+{
+    // Make a keff tally
+    auto keff = std::make_shared<Tally_t>(1.0, physics);
+
+    // Make a particle
+    auto p = std::make_shared<Particle_t>();
+
+    // Create reference variables
+    double ref_k     = 0.0;
+    double ref_kavg  = 0.0;
+    double ref_kmom1 = 0.0;
+    double ref_kmom2 = 0.0;
+    double ref_kvar  = 0.0;
+
+    /*** Begin INACTIVE CYCLE 1 ***/
+
+    keff->begin_cycle();
+    EXPECT_EQ(0.0, keff->latest());
+
+    // Tally a particle
+    p->set_wt(0.65);
+    p->set_matid(1);
+    p->set_group(1);
+    keff->accumulate(1.4, *p);
+    ref_k = 1.4 * 0.65 * 2.4 * 4.2;
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    // Put particle in group 2
+    p->set_group(2);  // no fission in group 2
+    keff->accumulate(1.8, *p);
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    // Put particle in group 0
+    p->set_group(0);
+    p->set_wt(0.4);
+    keff->accumulate(0.6, *p);
+    ref_k += 0.4 * 0.6 * 2.4 * 3.2;
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    // Put particle in material 0
+    p->set_matid(0);
+    p->set_wt(0.55);
+    keff->accumulate(0.2, *p);
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    // End the cycle
+    keff->end_cycle(3.0);
+    ref_k = nodes * ref_k / 3.0;
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    /*** Begin INACTIVE CYCLE 2 ***/
+
+    keff->begin_cycle();
+    EXPECT_EQ(0.0, keff->latest());
+
+    // Tally more particles
+    p->set_wt(0.55);
+    p->set_matid(1);
+    p->set_group(1);
+    keff->accumulate(1.4, *p);
+    ref_k = 1.4 * 0.55 * 2.4 * 4.2;
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    // Put particle in group 2
+    p->set_group(2);  // no fission in group 2
+    keff->accumulate(1.8, *p);
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    // Put particle in group 0
+    p->set_group(0);
+    p->set_wt(0.2);
+    keff->accumulate(0.6, *p);
+    ref_k += 0.2 * 0.6 * 2.4 * 3.2;
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    // Put particle in material 0
+    p->set_matid(0);
+    p->set_wt(0.55);
+    keff->accumulate(0.2, *p);
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    // End the cycle
+    keff->end_cycle(3.0);
+    ref_k = nodes * ref_k / 3.0;
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    /*** Begin ACTIVE CYCLE 1 ***/
+
+    keff->begin_active_cycles();
+    keff->begin_cycle();
+    EXPECT_EQ(0.0, keff->latest());
+    EXPECT_EQ(0.0, keff->keff_sum());
+    EXPECT_EQ(0.0, keff->keff_sum_sq());
+
+    // Tally some particles
+    p->set_wt(0.65);
+    p->set_matid(1);
+    p->set_group(1);
+    keff->accumulate(1.4, *p);
+    ref_k = 1.4 * 0.65 * 2.4 * 4.2;
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    // Put particle in group 2
+    p->set_group(2);  // no fission in group 2
+    keff->accumulate(1.8, *p);
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    // Put particle in group 0
+    p->set_group(0);
+    p->set_wt(0.4);
+    keff->accumulate(0.6, *p);
+    ref_k += 0.4 * 0.6 * 2.4 * 3.2;
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    // Put particle in material 0
+    p->set_matid(0);
+    p->set_wt(0.55);
+    keff->accumulate(0.2, *p);
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    // End the cycle
+    keff->end_cycle(3.0);
+
+    ref_k = nodes * ref_k / 3.0;
+    ref_kmom1 += ref_k;
+    ref_kmom2 += ref_k * ref_k;
+    ref_kavg = ref_kmom1 / 1.0;
+
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+    EXPECT_SOFTEQ(ref_kavg, keff->mean(), 1.0e-6);
+
+    /*** Begin ACTIVE CYCLE 2 ***/
+
+    keff->begin_cycle();
+    EXPECT_EQ(0.0, keff->latest());
+    EXPECT_NE(0.0, keff->keff_sum());
+    EXPECT_NE(0.0, keff->keff_sum_sq());
+
+    // Tally some more particles
+    p->set_wt(0.55);
+    p->set_matid(1);
+    p->set_group(1);
+    keff->accumulate(1.4, *p);
+    ref_k = 1.4 * 0.55 * 2.4 * 4.2;
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    // Put particle in group 2
+    p->set_group(2);  // no fission in group 2
+        keff->accumulate(1.8, *p);
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    // Put particle in group 0
+    p->set_group(0);
+    p->set_wt(0.2);
+    keff->accumulate(0.6, *p);
+    ref_k += 0.2 * 0.6 * 2.4 * 3.2;
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    // Put particle in material 0
+    p->set_matid(0);
+    p->set_wt(0.55);
+    keff->accumulate(0.2, *p);
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    // End the cycle
+    keff->end_cycle(3.0);
+
+    ref_k = nodes * ref_k / 3.0;
+    ref_kmom1 += ref_k;
+    ref_kmom2 += ref_k * ref_k;
+    ref_kavg = ref_kmom1 / 2.0;
+    ref_kvar = (ref_kmom2 - 2.0 * ref_kavg * ref_kavg) / 2.0 / 1.0;
+
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+    EXPECT_SOFTEQ(ref_kavg, keff->mean(), 1.0e-6);
+    EXPECT_SOFTEQ(ref_kvar, keff->variance(), 1.0e-6);
+
+    /*** Begin ACTIVE CYCLE 3 ***/
+
+    keff->begin_cycle();
+    EXPECT_EQ(0.0, keff->latest());
+    EXPECT_NE(0.0, keff->keff_sum());
+    EXPECT_NE(0.0, keff->keff_sum_sq());
+
+    // Tally some more particles
+    p->set_wt(0.8);
+    p->set_matid(1);
+    p->set_group(1);
+    keff->accumulate(1.4, *p);
+    ref_k = 1.4 * 0.8 * 2.4 * 4.2;
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    // Put particle in group 2
+    p->set_group(2);  // no fission in group 2
+    keff->accumulate(1.8, *p);
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    // Put particle in group 0
+    p->set_group(0);
+    p->set_wt(0.65);
+    keff->accumulate(0.6, *p);
+    ref_k += 0.65 * 0.6 * 2.4 * 3.2;
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    // Put particle in material 0
+    p->set_matid(0);
+    p->set_wt(0.55);
+    keff->accumulate(0.2, *p);
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+
+    // End the cycle
+    keff->end_cycle(3.0);
+
+    ref_k = nodes * ref_k / 3.0;
+    ref_kmom1 += ref_k;
+    ref_kmom2 += ref_k * ref_k;
+    ref_kavg = ref_kmom1 / 3.0;
+    ref_kvar = (ref_kmom2 - 3.0 * ref_kavg * ref_kavg) / 3.0 / 2.0;
+
+    EXPECT_SOFTEQ(ref_k, keff->latest(), 1.0e-6);
+    EXPECT_SOFTEQ(ref_kavg, keff->mean(), 1.0e-6);
+    EXPECT_SOFTEQ(ref_kvar, keff->variance(), 1.0e-6);
 }
 
 //---------------------------------------------------------------------------//
