388e71e2f1172b77829fa711447331566850964f
Steven Hamilton <hamiltonsp@ornl.gov>
2014-08-21 08:36:08 -0400

    Tpetra conversion of composite operators.

---
+++ packages/SPn/solvers/ShiftedOperator.pt.cc
@@ -0,0 +1,31 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   solvers/ShiftedOperator.pt.cc
+ * \author Thomas M. Evans
+ * \date   Fri Feb 21 14:41:20 2014
+ * \brief  ShiftedOperator explicit instantiation.
+ * \note   Copyright (C) 2014 Oak Ridge National Laboratory, UT-Battelle, LLC.
+ */
+//---------------------------------------------------------------------------//
+
+#include "Epetra_Operator.h"
+#include "Epetra_MultiVector.h"
+#include "AnasaziEpetraAdapter.hpp"
+#include "AnasaziTpetraAdapter.hpp"
+#include "ShiftedOperator.t.hh"
+
+namespace profugus
+{
+
+template class ShiftedOperator<Epetra_MultiVector,Epetra_Operator>;
+
+typedef KokkosClassic::SerialNode Node;
+typedef Tpetra::MultiVector<double,int,int,Node> MV;
+typedef Tpetra::Operator<double,int,int,Node> OP;
+template class ShiftedOperator<MV,OP>;
+
+} // end namespace profugus
+
+//---------------------------------------------------------------------------//
+//                 end of ShiftedOperator.pt.cc
+//---------------------------------------------------------------------------//
