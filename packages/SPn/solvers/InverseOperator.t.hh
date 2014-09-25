2104f9ff52041d4069fe7c2cf1af75ce7153b067
Steven Hamilton <hamiltonsp@ornl.gov>
2014-09-25 09:57:32 -0400

    More checking for invalid data in InverseOperator.

--- packages/SPn/solvers/InverseOperator.t.hh
+++ packages/SPn/solvers/InverseOperator.t.hh
@@ -85,13 +85,24 @@ void InverseOperatorBase<T>::set_preconditioner( Teuchos::RCP<OP> P )
 template <class T>
 void InverseOperatorBase<T>::ApplyImpl(const MV &x, MV &y ) const
 {
-    // If y comes in with invalid data, initialize it to zero
-    // Otherwise, leave it as is -- it may have a useful initial
-    //  guess for the linear solve (e.g. if we're using this in
-    //  power iteration)
+    // The data in the output vector y will be used as an initial guess
+    //  to the linear solver, which is a good idea if we're using this
+    //  inside of power iteration, in which case y will be a very good
+    //  approximation to the solution when the eigensolver is nearly
+    //  converged.  However, if this appears inside Arnoldi, y may
+    //  have uninitialized data in it (thanks Anasazi), which could
+    //  cause problems for the linear solver.  So we try to identify
+    //  "bad" data on input and initialize y to zero in this case.
+    //  We can't just look for inf or nan data, values that are "large"
+    //  (i.e. 10^80) will cause problems for the linear solver.  We set
+    //  an arbitrary threshold and if the vector norm exceeds this value
+    //  we'll reset it to zero.  This is a bit of a hack but I don't have
+    //  a better alternative to support our use cases.
+    double big_value = 1e10;
     std::vector<double> nrm(1);
     MVT::MvNorm(y,nrm);
-    if( Teuchos::ScalarTraits<typename T::ST>::isnaninf(nrm[0]) )
+    if( Teuchos::ScalarTraits<typename T::ST>::isnaninf(nrm[0]) ||
+        nrm[0] > big_value )
     {
         MVT::MvInit(y,0.0);
     }
