6bc7b96a1d3376bb1d8018cf9bef682be4075937
Tom Evans <evanstm@ornl.gov>
2014-10-28 12:13:12 -0400

    Updated simple ACC test.

--- packages/MC/acc/cmake/Dependencies.cmake
+++ packages/MC/acc/cmake/Dependencies.cmake
@@ -1,8 +1,25 @@
-SET(LIB_REQUIRED_DEP_PACKAGES)
+##---------------------------------------------------------------------------##
+## MC/acc/cmake/Dependencies.cmake
+## Thomas M. Evans
+## Saturday July 14 16:54:41 2012
+##---------------------------------------------------------------------------##
+
+##---------------------------------------------------------------------------##
+## Package requirements
+##---------------------------------------------------------------------------##
+
+SET(LIB_REQUIRED_DEP_PACKAGES
+  Teuchos Matprop Utils)
+
 SET(LIB_OPTIONAL_DEP_PACKAGES)
+
 SET(TEST_REQUIRED_DEP_PACKAGES)
 SET(TEST_OPTIONAL_DEP_PACKAGES)
 SET(LIB_REQUIRED_DEP_TPLS)
 SET(LIB_OPTIONAL_DEP_TPLS)
 SET(TEST_REQUIRED_DEP_TPLS)
 SET(TEST_OPTIONAL_DEP_TPLS)
+
+##---------------------------------------------------------------------------##
+##                  end of MC/acc/cmake/Dependencies.cmake
+##---------------------------------------------------------------------------##
