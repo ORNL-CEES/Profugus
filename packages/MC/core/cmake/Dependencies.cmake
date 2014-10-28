bc6df499c8c97d6291a6d39cbb7965276280f468
Tom Evans <evanstm@ornl.gov>
2014-10-28 09:19:23 -0400

    Made core package.

---
+++ packages/MC/core/cmake/Dependencies.cmake
@@ -0,0 +1,29 @@
+##---------------------------------------------------------------------------##
+## MC/cmake/Dependencies.cmake
+## Thomas M. Evans
+## Saturday July 14 16:54:41 2012
+##---------------------------------------------------------------------------##
+
+##---------------------------------------------------------------------------##
+## Define sub-packages
+##---------------------------------------------------------------------------##
+
+SET(SUBPACKAGES_DIRS_CLASSIFICATIONS_OPTREQS
+  ACC  acc  SS OPTIONAL
+  )
+
+SET(LIB_REQUIRED_DEP_PACKAGES
+  Teuchos Epetra Matprop Utils)
+
+SET(LIB_OPTIONAL_DEP_PACKAGES)
+
+SET(TEST_REQUIRED_DEP_PACKAGES)
+SET(TEST_OPTIONAL_DEP_PACKAGES)
+SET(LIB_REQUIRED_DEP_TPLS)
+SET(LIB_OPTIONAL_DEP_TPLS)
+SET(TEST_REQUIRED_DEP_TPLS)
+SET(TEST_OPTIONAL_DEP_TPLS)
+
+##---------------------------------------------------------------------------##
+##                  end of MC/cmake/Dependencies.cmake
+##---------------------------------------------------------------------------##
