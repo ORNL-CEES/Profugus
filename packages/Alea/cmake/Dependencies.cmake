f405ce6505c675ad522e2723fd3c1fc29a1db38e
Steven Hamilton <hamiltonsp@ornl.gov>
2015-02-24 11:51:27 -0500

    Adding explicit dependency on Teuchos.

--- packages/Alea/cmake/Dependencies.cmake
+++ packages/Alea/cmake/Dependencies.cmake
@@ -9,7 +9,7 @@
 ##---------------------------------------------------------------------------##
 
 SET(LIB_REQUIRED_DEP_PACKAGES
-    Utils SPn Tpetra Ifpack2 KokkosCore KokkosAlgorithms)
+    Utils SPn Teuchos Tpetra Ifpack2 KokkosCore KokkosAlgorithms)
 
 SET(LIB_OPTIONAL_DEP_PACKAGES)
 SET(TEST_REQUIRED_DEP_PACKAGES)
