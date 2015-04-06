ddefbe6bd5a7b80ffcd1713e720fd930377fda0d
Steven Hamilton <hamiltonsp@ornl.gov>
2015-04-06 08:55:02 -0400

    Adding wrapper to Thyra ModelEvaluator.

---
+++ packages/SPn/solvers/ThyraTraits.cc
@@ -0,0 +1,20 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   solvers/ThyraTraits.cc
+ * \author hu4
+ * \date   Thu Apr 02 10:24:48 2015
+ * \brief  ThyraTraits member definitions.
+ * \note   Copyright (C) 2015 Oak Ridge National Laboratory, UT-Battelle, LLC.
+ */
+//---------------------------------------------------------------------------//
+
+#include "ThyraTraits.hh"
+
+namespace profugus
+{
+
+} // end namespace profugus
+
+//---------------------------------------------------------------------------//
+//                 end of ThyraTraits.cc
+//---------------------------------------------------------------------------//
---
+++ packages/SPn/solvers/ThyraTraits.i.hh
@@ -0,0 +1,23 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   solvers/ThyraTraits.i.hh
+ * \author hu4
+ * \date   Thu Apr 02 10:24:48 2015
+ * \brief  Member definitions of class ThyraTraits.
+ * \note   Copyright (C) 2015 Oak Ridge National Laboratory, UT-Battelle, LLC.
+ */
+//---------------------------------------------------------------------------//
+
+#ifndef solvers_ThyraTraits_i_hh
+#define solvers_ThyraTraits_i_hh
+
+namespace profugus
+{
+
+} // end namespace profugus
+
+#endif // solvers_ThyraTraits_i_hh
+
+//---------------------------------------------------------------------------//
+//                 end of ThyraTraits.i.hh
+//---------------------------------------------------------------------------//
