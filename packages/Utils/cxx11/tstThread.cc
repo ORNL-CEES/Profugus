b182463c0c8595b4f9df0d8078662bd67a74a564
Slattery, Stuart R <slatterysr@ornl.gov>
2015-09-24 10:31:17 -0400

    updating cxx11 thread test to avoid race conditions

--- packages/Utils/cxx11/tstThread.cc
+++ packages/Utils/cxx11/tstThread.cc
@@ -24,25 +24,27 @@ TEST(Thread, function1)
     std::vector<int> x = {10, 22, 31, 44, 56};
 
     // run a thread to find first odd
-    auto odd_func = [&]( std::vector<int>& vec )
+    int first_odd = -1;
+    auto odd_func = []( int& i, std::vector<int>& vec )
 		    {
-			std::find_if(std::begin(vec), std::end(vec),
-				     [](int n){ return n % 2 == 1; });
+			i =
+			*std::find_if(std::begin(vec), std::end(vec),
+				      [](int n){ return n % 2 == 1; });
 		    };
-    std::thread odd_thread( odd_func, x );
-    EXPECT_EQ(31, *i);
+    std::thread odd_thread( odd_func, std::ref(first_odd), std::ref(x) );
 
     // run a thread to find first even
-    auto even_func = [&] ( std::vector<int>& vec )
+    int first_even = -1;
+    auto even_func = []( int& i, std::vector<int>& vec )
 		     {
-			 std::find_if(std::begin(vec), std::end(vec),
-				      [](int n){ return n % 2 == 0; });
-		     }
-    std::thread even_thread( even_func, x );
-    EXPECT_EQ(10, *i);
-
+			 i =
+			 *std::find_if(std::begin(vec), std::end(vec),
+				       [](int n){ return n % 2 == 0; });
+		     };
+    std::thread even_thread( even_func, std::ref(first_even), std::ref(x) );
+    
     // check that the threads have different ids
-    EXPECT_NEQ( odd_thread.get_id(), even_thread.get_id() );
+    EXPECT_NE( odd_thread.get_id(), even_thread.get_id() );
     
     // join the threads before exiting
     EXPECT_TRUE( odd_thread.joinable() );
@@ -51,6 +53,11 @@ TEST(Thread, function1)
     even_thread.join();
     EXPECT_FALSE( odd_thread.joinable() );
     EXPECT_FALSE( even_thread.joinable() );
+
+    // check the even and odd results after the threads have been joined to
+    // ensure the computation has been completed.
+    EXPECT_EQ(31, first_odd);
+    EXPECT_EQ(10, first_even );
 }
 
 //---------------------------------------------------------------------------//
