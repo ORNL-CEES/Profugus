1f372c002891e01546e67d6a10fa5d60722b0b76
Stuart Slattery <slatterysr@ornl.gov>
2015-10-07 17:04:16 -0400

    adding futurization to matrix vector multiply test

--- packages/Utils/hpx/tstMatrixVectorMultiply.cc
+++ packages/Utils/hpx/tstMatrixVectorMultiply.cc
@@ -43,7 +43,7 @@ class Matrix
     Matrix( const int num_rows, const int num_cols )
 	: d_num_rows( num_rows )
 	, d_num_cols( num_cols )
-	, d_data( num_rows, std::vector<double>(num_cols, 0.0) )
+	, d_data( num_cols, std::vector<double>(num_rows, 0.0) )
     { /* ... */ }
 
     // Get the number of rows.
@@ -55,13 +55,13 @@ class Matrix
     // Set a specific value.
     void set( const int row, const int col, const double value )
     {
-	d_data[row][col] = value;
+	d_data[col][row] = value;
     }
 
     // Get a specific value.
     double get( const int row, const int col ) const
     {
-	return d_data[row][col];
+	return d_data[col][row];
     }
 
     // Add. this = x + y
@@ -71,11 +71,11 @@ class Matrix
 	HPX_ASSERT( d_num_cols == x.d_num_cols );
 	HPX_ASSERT( d_num_rows == y.d_num_rows );
 	HPX_ASSERT( d_num_cols == y.d_num_cols );
-	for ( int i = 0; i < d_num_rows; ++i )
+	for ( int j = 0; j < d_num_cols; ++j )
 	{
-	    for ( int j = 0; j < d_num_cols; ++j )
+	    for ( int i = 0; i < d_num_rows; ++i )
 	    {
-		d_data[i][j] = x.d_data[i][j] + y.d_data[i][j];
+		d_data[j][i] = x.d_data[j][i] + y.d_data[j][i];
 	    }
 	}
     }
@@ -89,7 +89,7 @@ class Matrix
 	double sum = 0.0;
 	for ( int i = 0; i < d_num_rows; ++i )
 	{
-	    sum += d_data[i][0] * x.d_data[i][0];
+	    sum += d_data[0][i] * x.d_data[0][i];
 	}
 	return sum;
     }
@@ -100,17 +100,31 @@ class Matrix
 	HPX_ASSERT( d_num_rows == y.d_num_rows  );
 	HPX_ASSERT( d_num_cols == x.d_num_rows  );
 	HPX_ASSERT( x.d_num_cols == y.d_num_cols );
-	for ( int i = 0; i < d_num_rows; ++i )
+
+	// Create a vector of futures.
+	std::vector<hpx::future<void> > sum_futures;
+	sum_futures.reserve( d_num_rows );
+
+	// Create a sum operator for the threads.
+	auto sum_op = [this,&x,&y=y]( const int row, const int col )
 	{
-	    for ( int v = 0; v < x.d_num_cols; ++v )
+	    y.d_data[col][row] = 0.0;
+	    for ( int j = 0; j < this->d_num_cols; ++j ) 
+		y.d_data[col][row] += this->d_data[j][row] * x.d_data[col][j];
+
+	};
+
+	// Calculate the sum. Synchronize on each vector.
+	for ( int v = 0; v < x.d_num_cols; ++v )
+	{
+	    // Launch the sums. One task per row.
+	    for ( int i = 0; i < d_num_rows; ++i )
 	    {
-		y.d_data[i][v] = 0.0;
-		for ( int j = 0; j < d_num_cols; ++j )
-		{
-		    y.d_data[i][v] += 
-			d_data[i][j] * x.d_data[j][v];
-		}
+		sum_futures.push_back( hpx::async(sum_op, i, v) );
 	    }
+
+	    // Wait on the sums.
+	    hpx::lcos::wait_all(sum_futures);
 	}
     }
 
@@ -131,7 +145,7 @@ class Matrix
     // Number of columns.
     int d_num_cols;
 
-    // Local matrix data.
+    // Local matrix data. Column-major ordering.
     std::vector<std::vector<double> > d_data;
 };
 
@@ -151,6 +165,7 @@ TEST( matrix_vector, vector_dot_test )
     }
 
     EXPECT_EQ( x.dot(y), N );
+    EXPECT_EQ( x.norm2(), std::sqrt(N) );
 }
 
 //---------------------------------------------------------------------------//
