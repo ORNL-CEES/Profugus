66215ff385078f31438e8034c27770eb4d6d5b2d
Steven Hamilton <hamiltonsp@ornl.gov>
2015-12-15 16:20:45 -0500

    Initial implementation of Mesh_Geometry, not complete yet.

---
+++ packages/MC/cuda_geometry/Mesh_Geometry.cu
@@ -0,0 +1,28 @@
+//---------------------------------*-C++-*-----------------------------------//
+/*!
+ * \file   MC/cuda_geometry/Mesh_Geometry.cu
+ * \author Steven Hamilton
+ * \date   Tue Dec 15 14:10:10 2015
+ * \brief  Mesh_Geometry class definitions.
+ * \note   Copyright (c) 2015 Oak Ridge National Laboratory, UT-Battelle, LLC.
+ */
+//---------------------------------------------------------------------------//
+
+#include "Mesh_Geometry.hh"
+
+namespace cuda_profugus
+{
+//---------------------------------------------------------------------------//
+Mesh_Geometry::Mesh_Geometry(const Vec_Dbl &x_edges,
+                             const Vec_Dbl &y_edges,
+                             const Vec_Dbl &z_edges)
+    : d_mesh(x_edges,y_edges,z_edges)
+{
+}
+
+//---------------------------------------------------------------------------//
+} // end namespace cuda_profugus
+
+//---------------------------------------------------------------------------//
+// end of MC/cuda_geometry/Mesh_Geometry.cu
+//---------------------------------------------------------------------------//
