e55fd8cbf94e628ad0cd54dc0fabefe6be3e5915
Tom Evans <evanstm@ornl.gov>
2016-01-13 10:34:22 -0500

    Updated cuda install scripts.

--- install/cmake_emmet_cuda_kokkos.sh
+++ install/cmake_emmet_cuda_kokkos.sh
@@ -20,7 +20,7 @@ MPI_PATH="/usr/lib64/openmpi"
 HDF5_PATH="${MPI_PATH}"
 
 # Cuda flags
-CUDA_NVCC_FLAGS="-arch=sm_35;-std=c++11;-Xcudafe;--diag_suppress=code_is_unreachable"
+CUDA_NVCC_FLAGS="-std=c++11 -Xcudafe --diag_suppress=code_is_unreachable"
 
 ##---------------------------------------------------------------------------##
 
@@ -32,6 +32,8 @@ cmake \
 -DMPI_BASE_DIR:PATH=${MPI_PATH} \
 \
 -DCMAKE_C_COMPILER="${MPI_PATH}/bin/mpicc" \
+-DCMAKE_CXX_COMPILER="${PWD}/nvcc_wrapper" \
+-DCMAKE_CXX_FLAGS="-Wno-unused-local-typedefs -lineinfo -Xcudafe --diag_suppress=code_is_unreachable" \
 \
 -DTPL_ENABLE_HDF5:BOOL=ON \
 -DHDF5_INCLUDE_DIRS:PATH=${HDF5_PATH}/include \
@@ -42,25 +44,24 @@ cmake \
 \
 -DTPL_ENABLE_CUDA:BOOL=ON \
 -DCUDA_NVCC_FLAGS:STRING=${CUDA_NVCC_FLAGS} \
--DCUDA_PROPAGATE_HOST_FLAGS:BOOL=OFF \
 \
 -DProfugus_ENABLE_CXX11:BOOL=ON \
 -DProfugus_ENABLE_SPn:BOOL=ON \
--DProfugus_ENABLE_Alea:BOOL=OFF \
--DProfugus_ENABLE_MC:BOOL=ON \
+-DProfugus_ENABLE_Alea:BOOL=ON \
+-DProfugus_ENABLE_MC:BOOL=OFF \
 -DProfugus_ENABLE_Utils:BOOL=ON \
 -DProfugus_ENABLE_OpenMP:BOOL=ON \
 -DProfugus_ENABLE_Teko:BOOL=OFF \
 -DProfugus_ENABLE_Kokkos:BOOL=ON \
 -DProfugus_ENABLE_Tpetra:BOOL=ON \
 -DTpetra_ENABLE_TESTS:BOOL=OFF \
--DProfugus_ENABLE_MueLu:BOOL=OFF \
 \
 -DTPL_ENABLE_Pthread:BOOL=ON \
--DKokkos_ENABLE_Cuda:BOOL=OFF \
 -DKokkos_ENABLE_Pthread:BOOL=ON \
 -DKokkos_ENABLE_OpenMP:BOOL=ON \
+-DKokkosClassic_DefaultNode:STRING="Kokkos::Compat::KokkosCudaWrapperNode" \
 -DKokkos_ENABLE_CXX11:BOOL=ON \
+-DKokkos_ENABLE_Cuda_UVM:BOOL=ON \
 \
 -DTpetra_INST_OPENMP:BOOL=ON \
 -DTpetra_INST_PTHREAD:BOOL=ON \
