daadd1a848ec13d925a4c1312e68efe976db0ba9
Steven Hamilton <hamiltonsp@ornl.gov>
2016-02-25 10:58:07 -0500

    Adding test for Source_Transporter, doesn't currently test anything due to lack of tallies.

---
+++ packages/MC/cuda_mc/test/Source_Transporter_Tester.cu
@@ -0,0 +1,72 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   cuda_mc/Source_Transporter_Tester.cc
+ * \author Steven Hamilton
+ * \date   Wed Jan 20 16:13:24 2016
+ * \brief  Source_Transporter_Tester member definitions.
+ * \note   Copyright (C) 2016 Oak Ridge National Laboratory, UT-Battelle, LLC.
+ */
+//---------------------------------------------------------------------------//
+
+#include "Source_Transporter_Tester.hh"
+#include "../Source_Transporter.hh"
+#include "../Uniform_Source.cuh"
+#include "../Physics.cuh"
+#include "cuda_geometry/Mesh_Geometry.hh"
+#include "CudaUtils/cuda_utils/Shared_Device_Ptr.hh"
+#include "Teuchos_RCP.hpp"
+#include "Teuchos_ParameterList.hpp"
+
+namespace cuda_mc
+{
+
+typedef cuda_profugus::Mesh_Geometry      Geom;
+typedef cuda_mc::Uniform_Source<Geom>     Uniform_Src;
+typedef cuda_mc::Source_Transporter<Geom> Transporter;
+
+void Source_Transporter_Tester::test_transport( const Vec_Dbl  &x_edges,
+                                                const Vec_Dbl  &y_edges,
+                                                const Vec_Dbl  &z_edges,
+                                                const Vec_UInt &matids,
+                                                      SP_XS     xs,
+                                                      int       num_particles)
+{
+    // Build geometry
+    auto geom = std::make_shared<Geom>(x_edges,y_edges,z_edges);
+    geom->set_matids(matids);
+    cuda::Shared_Device_Ptr<cuda_profugus::Mesh_Geometry> sdp_geom(geom);
+
+    // Build physics
+    Teuchos::RCP<Teuchos::ParameterList> pl( new Teuchos::ParameterList() );
+    pl->set("num_groups",xs->num_groups());
+    pl->set("Np",num_particles);
+    pl->set("implicit_capture",false);
+    auto phys = std::make_shared<Physics<Geom> >(pl,xs);
+    phys->set_geometry(sdp_geom);
+    cuda::Shared_Device_Ptr<Physics<Geom> > sdp_phys(phys);
+
+    // Build box shape for source
+    Vec_Dbl src_bounds = {x_edges.front(), x_edges.back(),
+                          y_edges.front(), y_edges.back(),
+                          z_edges.front(), z_edges.back()};
+    REQUIRE( src_bounds.size() == 6 );
+    auto src_shape = std::make_shared<cuda_mc::Box_Shape>(
+            src_bounds[0], src_bounds[1],
+            src_bounds[2], src_bounds[3],
+            src_bounds[4], src_bounds[5]);
+
+    // Build source
+    auto source = std::make_shared<Uniform_Src>(pl,sdp_geom);
+    source->build_source(src_shape);
+
+    // Build source transporter
+    Transporter trans(pl,sdp_geom,sdp_phys);
+    trans.solve(source);
+}
+
+} // end namespace cuda_mc
+
+//---------------------------------------------------------------------------//
+//                 end of Source_Transporter_Tester.cc
+
+//---------------------------------------------------------------------------//
---
+++ packages/MC/cuda_mc/test/Source_Transporter_Tester.hh
@@ -0,0 +1,51 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   cuda_mc/Source_Transporter_Tester.hh
+ * \author Steven Hamilton
+ * \date   Wed Jan 20 16:13:24 2016
+ * \brief  Source_Transporter_Tester class definition.
+ * \note   Copyright (C) 2016 Oak Ridge National Laboratory, UT-Battelle, LLC.
+ */
+//---------------------------------------------------------------------------//
+
+#ifndef cuda_mc_Source_Transporter_Tester_hh
+#define cuda_mc_Source_Transporter_Tester_hh
+
+#include <vector>
+#include <memory>
+#include "Matprop/xs/XS.hh"
+
+namespace cuda_mc
+{
+
+//===========================================================================//
+/*!
+ * \class Source_Transporter_Tester
+ * \brief Wrapper to kernel launches to test Source_Transporter class.
+ */
+//===========================================================================//
+
+class Source_Transporter_Tester
+{
+  public:
+
+      typedef std::vector<int>              Vec_Int;
+      typedef std::vector<unsigned int>     Vec_UInt;
+      typedef std::vector<double>           Vec_Dbl;
+      typedef std::shared_ptr<profugus::XS> SP_XS;
+
+      static void test_transport( const Vec_Dbl  &x_edges,
+                                  const Vec_Dbl  &y_edges,
+                                  const Vec_Dbl  &z_edges,
+                                  const Vec_UInt &matids,
+                                        SP_XS     xs,
+                                        int       num_particles );
+};
+
+} // end namespace cuda_mc
+
+#endif // cuda_mc_Source_Transporter_Tester_hh
+
+//---------------------------------------------------------------------------//
+//                 end of Source_Transporter_Tester.hh
+//---------------------------------------------------------------------------//
