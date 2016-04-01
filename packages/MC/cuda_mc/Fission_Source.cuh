0b5e813f94990ee47523273ed62c838cd47ea0e8
Steven Hamilton <hamiltonsp@ornl.gov>
2016-04-01 16:23:19 -0400

    First cut at fission source.

---
+++ packages/MC/cuda_mc/Fission_Source.cuh
@@ -0,0 +1,181 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   cuda_mc/Fission_Source.cuh
+ * \author Steven Hamilton
+ * \date   Mon May 05 14:22:46 2014
+ * \brief  Fission_Source class definition.
+ * \note   Copyright (C) 2014 Oak Ridge National Laboratory, UT-Battelle, LLC.
+ */
+//---------------------------------------------------------------------------//
+
+#ifndef cuda_mc_Fission_Source_cuh
+#define cuda_mc_Fission_Source_cuh
+
+#include <thrust/device_vector.h>
+
+#include "cuda_geometry/Cartesian_Mesh.hh"
+//#include "Fission_Rebalance.hh"
+#include "Physics.cuh"
+#include "Particle.cuh"
+#include "RNG_Control.cuh"
+#include "Source.cuh"
+
+namespace cuda_mc
+{
+
+//===========================================================================//
+/*!
+ * \class Fission_Source
+ * \brief Defines both an initial and within-cycle fission source for k-code
+ * calculations.
+ *
+ * Through the database passed into the fission source the client specifies
+ * the number of fission particles to run each cycle.  On cycles other than
+ * the first, the number of fission source particles is determined by the
+ * number of sites sampled during the previous generation.  To keep the
+ * effective number of particles constant between cycles, fission particles
+ * are born with the following weight,
+ * \f[
+   w = N_p / M\:,
+ * \f]
+ * where \f$N_p\f$ is the number of requested particles per cycle and \f$M\f$
+ * is the total number of particles sampled from the fission source.  In the
+ * first cycle \f$M = N_p\f$; however, in various parallel decompositions the
+ * number of particles run in the first cycle may be slightly less than the
+ * number of particles requested (but never more).  Thus, even in the first
+ * cycle the weight of particles may be slightly greater than one because the
+ * weight per particle is normalized to the \b requested number of particles
+ * per cycle.  Using this weight preserves the constant total weight per
+ * cycle.  The requested number of particles can be queried through the Np()
+ * member function.
+ *
+ * \section fission_source_db Standard DB Entries for Fission_Source
+ *
+ * The database entries that control the profugus::Fission_Source are:
+ *
+ * \arg \c init_fission_src (vector<double>) box defined as (lox, hix, loy,
+ * hiy, loz, hiz) in which the initial fission source is sampled (default: the
+ * entire geometry)
+ *
+ * \arg \c Np (int) number of particles to use in each cycle (default:
+ * 1000)
+ */
+/*!
+ * \example mc/test/tstFission_Source.cc
+ *
+ * Test of Fission_Source.
+ */
+//===========================================================================//
+
+template <class Geometry>
+class Fission_Source : public Source<Geometry>
+{
+  public:
+    //@{
+    //! Typedefs.
+    typedef Geometry                                    Geometry_t;
+    typedef Physics<Geometry_t>                         Physics_t;
+    typedef typename Physics_t::Fission_Site            Fission_Site;
+    typedef thrust::device_vector<Fission_Site>         Fission_Site_Vector;
+    typedef Particle<Geometry_t>                        Particle_t;
+    typedef cuda::Space_Vector                          Space_Vector;
+    typedef RNG_Control::RNG_State_t                    RNG_State_t;
+    typedef cuda::Shared_Device_Ptr<Geometry>           SDP_Geometry;
+    typedef cuda::Shared_Device_Ptr<Physics_t>          SDP_Physics;
+    //typedef Fission_Rebalance<Geometry_t>               Fission_Rebalance_t;
+    //typedef std::shared_ptr<Fission_Rebalance_t>        SP_Fission_Rebalance;
+    typedef Teuchos::RCP<Teuchos::ParameterList>        RCP_Std_DB;
+    typedef def::size_type                              size_type;
+    //@}
+
+  private:
+    // >>> DATA
+
+    // Fission site container.
+    Fission_Site_Vector d_fission_site_vec;
+    Fission_Site       *d_fission_sites;
+
+    // Fission rebalance (across sets).
+    //SP_Fission_Rebalance d_fission_rebalance;
+
+    // Physics
+    SDP_Physics d_physics_host;
+    Physics_t  *d_physics;
+
+  public:
+    // Constructor.
+    Fission_Source(RCP_Std_DB db, SDP_Geometry geometry, SDP_Physics physics);
+
+    // Build the initial fission source.
+    void build_initial_source();
+
+    // Build a source from a fission site container.
+    void build_source(Fission_Site_Vector &fission_sites);
+
+    // >>> DERIVED PUBLIC INTERFACE
+
+    // Get a particle from the source.
+    __device__ Particle_t get_particle(std::size_t idx, RNG_State_t *rng) const;
+
+    //! Number of particles to transport in the source on the current domain.
+    __host__ __device__
+    size_type num_to_transport() const { return d_np_domain; }
+
+    //! Total number of particles to transport in the entire problem/cycle.
+    __host__ __device__ 
+    size_type total_num_to_transport() const { return d_np_total; }
+
+    // >>> CLASS ACCESSORS
+
+    //! Get the current fission site container.
+    const Fission_Site_Vector & fission_sites() const
+    {
+        return d_fission_site_vec;
+    }
+
+    //! Total number of requested particles per cycle.
+    __host__ __device__ size_type Np() const { return d_np_requested; }
+
+    //! Set a new number per cycle.
+    void update_Np(size_type np) { d_np_requested = np; }
+
+  private:
+    // >>> IMPLEMENTATION
+
+    typedef Source<Geometry> Base;
+    using Base::b_geometry;
+
+    // Build the domain replicated fission source.
+    void build_DR();
+
+    // Sample the geometry.
+    __device__
+    int sample_geometry(Space_Vector &r, const Space_Vector &omega,
+                        Particle_t &p, RNG_State_t *rng) const;
+
+    int d_nodes;
+
+    // Have we populated fission_sites yet?
+    bool d_have_sites;
+
+    // Initial fission source lower coords and width.
+    Space_Vector d_lower;
+    Space_Vector d_width;
+
+    // Requested particles per cycle.
+    size_type d_np_requested;
+
+    // Number of particles: total, domain
+    size_type d_np_total, d_np_domain;
+
+    // Particle weight.
+    double d_wt;
+};
+
+} // end namespace cuda_mc
+
+#endif // cuda_mc_Fission_Source_cuh
+
+//---------------------------------------------------------------------------//
+//                 end of Fission_Source.cukh
+//---------------------------------------------------------------------------//
---
+++ packages/MC/cuda_mc/Fission_Source.t.cuh
@@ -0,0 +1,332 @@
+//----------------------------------*-C++-*----------------------------------//
+/*!
+ * \file   cuda_mc/Fission_Source.t.cuh
+ * \author Steven Hamilton
+ * \date   Mon May 05 14:22:46 2014
+ * \brief  Fission_Source template member definitions.
+ * \note   Copyright (C) 2014 Oak Ridge National Laboratory, UT-Battelle, LLC.
+ */
+//---------------------------------------------------------------------------//
+
+#ifndef cuda_mc_Fission_Source_t_cuh
+#define cuda_mc_Fission_Source_t_cuh
+
+#include <algorithm>
+#include <numeric>
+
+#include "Teuchos_Array.hpp"
+
+#include "Fission_Source.cuh"
+#include "Sampler.cuh"
+
+#include "harness/DBC.hh"
+#include "harness/Diagnostics.hh"
+#include "comm/global.hh"
+#include "comm/Timing.hh"
+#include "utils/Constants.hh"
+
+namespace cuda_mc
+{
+
+//---------------------------------------------------------------------------//
+// CONSTRUCTOR
+//---------------------------------------------------------------------------//
+/*!
+ * \brief Constructor.
+ */
+template <class Geometry>
+Fission_Source<Geometry>::Fission_Source(RCP_Std_DB     db,
+                                         SDP_Geometry   geometry,
+                                         SDP_Physics    physics)
+    : Base(geometry)
+    //, d_fission_rebalance(std::make_shared<Fission_Rebalance_t>())
+    , d_physics_host(physics)
+    , d_nodes(profugus::nodes())
+    , d_have_sites(false)
+    , d_np_requested(0)
+    , d_np_total(0)
+    , d_np_domain(0)
+    , d_wt(0.0)
+{
+    using def::I; using def::J; using def::K;
+
+    REQUIRE(b_geometry);
+
+    REQUIRE(d_physics_host.get_host_ptr());
+    REQUIRE(d_physics_host.get_device_ptr());
+    d_physics = physics.get_device_ptr();
+
+    // Boundaries in -X, +X, -Y, +Y, -Z, +Z
+    Teuchos::Array<double> extents(6, 0.);
+
+    // Assign large extents that will be trimmed to the geometry by default
+    extents[0] = extents[2] = extents[4] = -std::numeric_limits<double>::max();
+    extents[1] = extents[3] = extents[5] =  std::numeric_limits<double>::max();
+
+    extents = db->get("init_fission_src", extents);
+
+    INSIST(extents.size() == 6, "Fission source must have 6 entries");
+
+    // get the low and upper bounds of the geometry
+    INSIST(false, "Need bounding box data from geometry.");
+    auto low_edge  = geometry.get_host_ptr()->lower();
+    auto high_edge = geometry.get_host_ptr()->upper();
+
+    // X Bounds
+    double lower = extents[0];
+    double upper = extents[1];
+
+    lower = std::max(lower, low_edge.x);
+    upper = std::min(upper, high_edge.x);
+
+    d_lower.x = lower;
+    d_width.x = upper - lower;
+
+    // Y Bounds
+    lower = extents[2];
+    upper = extents[3];
+
+    lower = std::max(lower, low_edge.y);
+    upper = std::min(upper, high_edge.y);
+
+    d_lower.y = lower;
+    d_width.y = upper - lower;
+
+    // Z Bounds
+    lower = extents[4];
+    upper = extents[5];
+
+    lower = std::max(lower, low_edge.z);
+    upper = std::min(upper, high_edge.z);
+
+    d_lower.z = lower;
+    d_width.z = upper - lower;
+
+    INSIST(d_width.x > 0., "Fission source x width is non-positive");
+    INSIST(d_width.y > 0., "Fission source y width is non-positive");
+    INSIST(d_width.z > 0., "Fission source z width is non-positive");
+
+    // store the total number of requested particles per cycle
+    d_np_requested = static_cast<size_type>(db->get("Np", 1000));
+    INSIST(d_np_requested > 0, "Number of source particles must be positive.");
+
+    // initialize the total for the first cycle
+    d_np_total = d_np_requested;
+}
+
+//---------------------------------------------------------------------------//
+// PUBLIC FUNCTIONS
+//---------------------------------------------------------------------------//
+/*!
+ * \brief Build the initial fission source.
+ */
+template <class Geometry>
+void Fission_Source<Geometry>::build_initial_source()
+{
+    REQUIRE(d_np_total > 0);
+
+    // build the domain-replicated fission source
+    build_DR();
+
+    // weight per particle
+    d_wt = static_cast<double>(d_np_requested) /
+           static_cast<double>(d_np_total);
+
+    profugus::global_barrier();
+
+    ENSURE(d_wt > 0.0);
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * \brief Build a source from a fission site container.
+ *
+ * \param fission_sites
+ */
+template <class Geometry>
+void Fission_Source<Geometry>::build_source(Fission_Site_Vector &fission_sites)
+{
+    REQUIRE(!fission_sites.empty());
+
+    // the internal fission source should be empty
+    REQUIRE(d_fission_site_vec.empty());
+
+    SCOPED_TIMER("MC::Fission_Source.build_source");
+
+    // swap the input fission sites with the internal storage fission sites
+    d_fission_site_vec.swap(fission_sites);
+
+    // rebalance across sets (when number of blocks per set > 1; the
+    // set-rebalance may try to do some load-balancing when it can, that is
+    // why this call should comm after the gather; otherwise the
+    // load-balancing could provide poor results)
+    //d_fission_rebalance->rebalance(d_fission_sites);
+
+    // get the number of fission sites on this domain, on this set, and
+    // globally from the fission-rebalance
+    //d_np_domain = d_fission_rebalance->num_fissions();
+    //d_np_total  = d_fission_rebalance->num_global_fissions();
+    d_np_domain = d_fission_site_vec.size();
+    d_np_total  = d_np_domain;
+    CHECK(d_np_domain >= d_fission_site_vec.size()); // there could be multiple
+                                                     // fissions at a single
+                                                     // site
+
+    // weight per particle
+    d_wt = static_cast<double>(d_np_requested) /
+           static_cast<double>(d_np_total);
+
+    profugus::global_barrier();
+
+    // Assign raw pointer for on-device access
+    d_fission_sites = d_fission_site_vec.data().get();
+    d_have_sites = true;
+
+    ENSURE(d_wt > 0.0);
+    ENSURE(fission_sites.empty());
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * \brief Get a particle from the source.
+*/
+template <class Geometry>
+__device__
+auto Fission_Source<Geometry>::get_particle(
+    std::size_t tid, RNG_State_t *rng) const -> Particle_t
+{
+    REQUIRE(d_wt > 0.0);
+
+    // particle
+    Particle_t p;
+    CHECK(!p.alive());
+
+    // use the global rng on this domain for the random number generator
+    p.set_rng(rng);
+
+    // material id
+    int matid = 0;
+
+    // particle position and isotropic direction
+    Space_Vector r, omega;
+
+    // sample the angle isotropically
+    sampler::sample_isotropic(omega, rng);
+
+    // sample flag
+    bool sampled;
+
+    // if there is a fission site container than get the particle from there;
+    // otherwise assume this is an initial source
+    if (d_have_sites)
+    {
+        // get the last element in the site container
+        Fission_Site &fs = d_fission_sites[tid];
+
+        // get the location of the physics site
+        r = d_physics->fission_site(fs);
+
+        // intialize the geometry state
+        b_geometry->initialize(r, omega, p.geo_state());
+
+        // get the material id
+        matid = b_geometry->matid(p.geo_state());
+
+        // initialize the physics state at the fission site
+        sampled = d_physics->initialize_fission(fs, p);
+        CHECK(sampled);
+    }
+    else
+    {
+        matid = sample_geometry(r, omega, p, rng);
+    }
+
+    // set the material id in the particle
+    p.set_matid(matid);
+
+    // set particle weight
+    p.set_wt(d_wt);
+
+    // make particle alive
+    p.live();
+
+    ENSURE(p.matid() == matid);
+    return p;
+}
+
+//---------------------------------------------------------------------------//
+// PRIVATE FUNCTIONS
+//---------------------------------------------------------------------------//
+/*!
+ * \brief Build the initial source for Domain Replicated decompositions.
+ *
+ * In DR decompositions the number of sets is equal to the number of domains
+ * (1 block per set).  Thus, the number of particles per set is equal to the
+ * number of particles per domain.
+ */
+template <class Geometry>
+void Fission_Source<Geometry>::build_DR()
+{
+    // calculate the number of particles per domain and set (equivalent)
+    d_np_domain = d_np_total / d_nodes;
+
+    // recalculate the total number of particles (we want the same number of
+    // particles in each domain, so the total may change slightly from the
+    // requested value)
+    d_np_total = d_np_domain * d_nodes;
+}
+
+//---------------------------------------------------------------------------//
+/*!
+ * \brief Sample geometry to get a particle.
+ */
+template <class Geometry>
+__device__
+int Fission_Source<Geometry>::sample_geometry(Space_Vector       &r,
+                                              const Space_Vector &omega,
+                                              Particle_t         &p,
+                                              RNG_State_t        *rng) const
+{
+    // sampled complete flag
+    bool sampled = false;
+
+    // material id
+    int matid = 0;
+
+    // >>> Sample the full geometry
+
+    // sample the geometry until a fission site is found (if there is no
+    // fission in a given domain the number of particles on that domain is
+    // zero, and we never get here) --> so, fission sampling should always
+    // be successful
+    while (!sampled)
+    {
+        // sample a point in the geometry
+        r.x = d_width.x * curand_uniform_double(rng) + d_lower.x;
+        r.y = d_width.y * curand_uniform_double(rng) + d_lower.y;
+        r.z = d_width.z * curand_uniform_double(rng) + d_lower.z;
+
+        // intialize the geometry state
+        b_geometry->initialize(r, omega, p.geo_state());
+
+        // get the material id
+        matid = b_geometry->matid(p.geo_state());
+
+        // try initializing fission here, if it is successful we are
+        // finished
+        if (d_physics->initialize_fission(matid, p))
+        {
+            sampled = true;
+        }
+    }
+
+    return matid;
+}
+
+} // end namespace cuda_mc
+
+#endif // cuda_mc_Fission_Source_t_cuh
+
+//---------------------------------------------------------------------------//
+//                 end of Fission_Source.t.cuh
+//---------------------------------------------------------------------------//
