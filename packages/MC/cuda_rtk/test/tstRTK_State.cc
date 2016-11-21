ed01b463a9e7f6c6e298532aae92ba58ee5a618a
Tom Evans <evanstm@ornl.gov>
2016-11-21 14:17:52 -0500

    Finished test.

--- packages/MC/cuda_rtk/test/tstRTK_State.cc
+++ packages/MC/cuda_rtk/test/tstRTK_State.cc
@@ -15,21 +15,26 @@
 // see RTK_State_Tester.cu for implementations
 //---------------------------------------------------------------------------//
 
+namespace rtk_state_test
+{
+
 void test_on_device();
 void test_to_device();
+
+} // end namespace rtk_state_test
 
 //---------------------------------------------------------------------------//
 
 TEST(RTK_StateTest, on_device)
 {
-    test_on_device();
+    rtk_state_test::test_on_device();
 }
 
 //---------------------------------------------------------------------------//
 
 TEST(RTK_StateTest, to_device)
 {
-    test_to_device();
+    rtk_state_test::test_to_device();
 }
 
 //---------------------------------------------------------------------------//
