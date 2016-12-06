4bc08741f61052100003472d3afaa338121842a0
Tom Evans <evanstm@ornl.gov>
2016-12-06 11:56:10 -0500

    Finished single-shell tests.

--- packages/MC/cuda_rtk/test/RTK_Cell_Tester.cu
+++ packages/MC/cuda_rtk/test/RTK_Cell_Tester.cu
@@ -10,6 +10,7 @@
 
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
+#include <thrust/fill.h>
 
 #include "RTK_Cell_Tester.hh"
 #include "../RTK_Cell.cuh"
@@ -97,8 +98,116 @@ void single_shell_kernel1(
 
 __global__
 void single_shell_kernel2(
-    Device_Cell pin)
+    Device_Cell  pin,
+    int         *ints,
+    double      *dbls)
 {
+    DEVICE_REQUIRE(ints[0] == -1);
+    DEVICE_REQUIRE(dbls[0] == -1.0);
+
+    State  state;
+    Vector r, omega;
+    int    n = 0, m = 0;
+
+    // Pin intersection tests
+    {
+        r     = {  0.43,   0.51,   1.20};
+        omega = { -0.07450781,  -0.17272265,   0.98214840};
+        pin.initialize(r, state);
+        ints[n++] = state.region;
+        pin.distance_to_boundary(r, omega, state);
+        ints[n++] = state.exiting_face;
+        ints[n++] = state.next_region;
+        dbls[m++] = state.dist_to_next_region;
+    }
+    {
+        r     = {  0.43,   0.51,   1.20};
+        omega = {  0.01923789,  -0.98113214,  -0.19237885};
+        pin.initialize(r, state);
+        ints[n++] = state.region;
+        pin.distance_to_boundary(r, omega, state);
+        ints[n++] = state.exiting_face;
+        ints[n++] = state.next_region;
+        dbls[m++] = state.dist_to_next_region;
+    }
+    {
+        r     = { -0.49,   0.10,   1.20};
+        omega = {  0.04377546,  -0.01122448,   0.99897834};
+        pin.initialize(r, state);
+        ints[n++] = state.region;
+        pin.distance_to_boundary(r, omega, state);
+        ints[n++] = state.exiting_face;
+        ints[n++] = state.next_region;
+        dbls[m++] = state.dist_to_next_region;
+    }
+    {
+        r     = { -0.31,  -0.46,   1.20};
+        omega = { -0.01642565,   0.05812155,   0.99817438};
+        pin.initialize(r, state);
+        ints[n++] = state.region;
+        pin.distance_to_boundary(r, omega, state);
+        ints[n++] = state.exiting_face;
+        ints[n++] = state.next_region;
+        dbls[m++] = state.dist_to_next_region;
+    }
+    {
+        r     = {  0.21,  -0.56,   1.20};
+        omega = { -0.03911234,   0.07065454,   0.99673375};
+        pin.initialize(r, state);
+        ints[n++] = state.region;
+        pin.distance_to_boundary(r, omega, state);
+        ints[n++] = state.exiting_face;
+        ints[n++] = state.next_region;
+        dbls[m++] = state.dist_to_next_region;
+    }
+    {
+        r     = {  0.21,  -0.56,  14.20};
+        omega = {  0.021262916894,   0.051770580263,   0.998432619351};
+        pin.initialize(r, state);
+        ints[n++] = state.region;
+        pin.distance_to_boundary(r, omega, state);
+        ints[n++] = state.exiting_face;
+        dbls[m++] = state.dist_to_next_region;
+    }
+
+    {
+        r     = {  0.42,   0.10,  14.20};
+        omega = { -0.262232636986,  -0.030141682412,  -0.964533837188};
+        pin.initialize(r, state);
+        ints[n++] = state.region;
+        pin.distance_to_boundary(r, omega, state);
+        ints[n++] = state.exiting_face;
+        ints[n++] = state.next_region;
+        dbls[m++] = state.dist_to_next_region;
+    }
+    {
+        r     = {  0.10,   0.30,  12.10};
+        omega = {  0.408248290464,   0.163299316186,  -0.898146239020};
+        pin.initialize(r, state);
+        ints[n++] = state.region;
+        pin.distance_to_boundary(r, omega, state);
+        ints[n++] = state.exiting_face;
+        ints[n++] = state.next_region;
+        dbls[m++] = state.dist_to_next_region;
+    }
+    {
+        r     = {  0.10,   0.30,  12.10};
+        omega = {  0.011875513070,   0.004750205228,  -0.999918200524};
+        pin.initialize(r, state);
+        ints[n++] = state.region;
+        pin.distance_to_boundary(r, omega, state);
+        ints[n++] = state.exiting_face;
+        dbls[m++] = state.dist_to_next_region;
+    }
+    {
+        r     = {  0.10,   0.30,  12.10};
+        omega = {  0.072244077132,   0.028897630853,   0.996968264415};
+        pin.initialize(r, state);
+        ints[n++] = state.region;
+        pin.distance_to_boundary(r, omega, state);
+        ints[n++] = state.exiting_face;
+        dbls[m++] = state.dist_to_next_region;
+    }
 }
 
 //---------------------------------------------------------------------------//
@@ -110,8 +219,8 @@ void Single_Shell::run()
     auto p1 = d1.device_instance();
     auto p2 = d2.device_instance();
 
-    thrust::device_vector<int>    ints(20, -1);
-    thrust::device_vector<double> dbls(20, -1);
+    thrust::device_vector<int>    ints(50, -1);
+    thrust::device_vector<double> dbls(50, -1);
     thrust::device_vector<Vector> svs(20);
 
     {
@@ -158,8 +267,65 @@ void Single_Shell::run()
         EXPECT_EQ(1, rints[17]);
     }
 
+    thrust::fill(ints.begin(), ints.end(), -1);
+    thrust::fill(dbls.begin(), dbls.end(), -1);
+
     {
-        single_shell_kernel2<<<1,1>>>(p2);
+        single_shell_kernel2<<<1,1>>>(
+            p2, ints.data().get(), dbls.data().get());
+
+        thrust::host_vector<int>    rints(ints.begin(), ints.end());
+        thrust::host_vector<double> rdbls(dbls.begin(), dbls.end());
+
+        int    n   = 0, m = 0;
+        double eps = 1.0e-6;
+
+        EXPECT_EQ(1, rints[n++]);
+        EXPECT_SOFTEQ(rdbls[m++], 1.2334036420, eps);
+        EXPECT_EQ(State::INTERNAL, rints[n++]);
+        EXPECT_EQ(0, rints[n++]);
+
+        EXPECT_EQ(1, rints[n++]);
+        EXPECT_SOFTEQ(rdbls[m++], 0.41448110826, eps);
+        EXPECT_EQ(State::INTERNAL, rints[n++]);
+        EXPECT_EQ(0, rints[n++]);
+
+        EXPECT_EQ(1, rints[n++]);
+        EXPECT_SOFTEQ(rdbls[m++], 1.1101358881, eps);
+        EXPECT_EQ(State::INTERNAL, rints[n++]);
+        EXPECT_EQ(0, rints[n++]);
+
+        EXPECT_EQ(1, rints[n++]);
+        EXPECT_SOFTEQ(rdbls[m++], 3.4103552300, eps);
+        EXPECT_EQ(State::INTERNAL, rints[n++]);
+        EXPECT_EQ(0, rints[n++]);
+
+        EXPECT_EQ(1, rints[n++]);
+        EXPECT_SOFTEQ(rdbls[m++], 1.860292469, eps);
+        EXPECT_EQ(State::INTERNAL, rints[n++]);
+        EXPECT_EQ(0, rints[n++]);
+
+        EXPECT_EQ(1, rints[n++]);
+        EXPECT_SOFTEQ(rdbls[m++], 0.08 / 0.998432619351, eps);
+        EXPECT_EQ(State::PLUS_Z, rints[n++]);
+
+        EXPECT_EQ(0, rints[n++]);
+        EXPECT_SOFTEQ(rdbls[m++], 3.3176648414, eps);
+        EXPECT_EQ(State::INTERNAL, rints[n++]);
+        EXPECT_EQ(1, rints[n++]);
+
+        EXPECT_EQ(0, rints[n++]);
+        EXPECT_SOFTEQ(rdbls[m++], 3.9914694397e-01, eps);
+        EXPECT_EQ(State::INTERNAL, rints[n++]);
+        EXPECT_EQ(1, rints[n++]);
+
+        EXPECT_EQ(0, rints[n++]);
+        EXPECT_SOFTEQ(rdbls[m++], -12.10 / -0.999918200524, eps);
+        EXPECT_EQ(State::MINUS_Z, rints[n++]);
+
+        EXPECT_EQ(0, rints[n++]);
+        EXPECT_SOFTEQ(rdbls[m++], 2.18 / 0.996968264415, eps);
+        EXPECT_EQ(State::PLUS_Z, rints[n++]);
     }
 }
 
