f8b597aea21ae110e63603744df913678227cd0b
Tom Evans <evanstm@ornl.gov>
2016-12-06 10:41:15 -0500

    Updated rtk cell tests for cuda.

--- packages/MC/cuda_rtk/test/RTK_Cell_Tester.cu
+++ packages/MC/cuda_rtk/test/RTK_Cell_Tester.cu
@@ -18,10 +18,10 @@
 // TYPES
 //---------------------------------------------------------------------------//
 
-using Device_Cell  = cuda_profugus::RTK_Cell;
-using DMM          = cuda_profugus::RTK_Cell_DMM;
-using Space_Vector = Device_Cell::Space_Vector;
-using State        = Device_Cell::Geo_State_t;
+using Device_Cell = cuda_profugus::RTK_Cell;
+using DMM         = cuda_profugus::RTK_Cell_DMM;
+using Vector      = Device_Cell::Space_Vector;
+using State       = Device_Cell::Geo_State_t;
 
 using def::X; using def::Y; using def::Z;
 
@@ -31,24 +31,66 @@ using def::X; using def::Y; using def::Z;
 
 __global__
 void single_shell_kernel1(
-    Device_Cell   pin,
-    int          *ints,
-    double       *dbls,
-    Space_Vector *svs)
+    Device_Cell  pin,
+    int         *ints,
+    double      *dbls,
+    Vector      *svs)
 {
-    auto r  = pin.radii();
-    dbls[0] = r[0];
-    dbls[1] = pin.pitch(X);
-    dbls[2] = pin.pitch(Y);
+    // Test basic pin parameters
+    {
+        auto r  = pin.radii();
+        dbls[0] = r[0];
+        dbls[1] = pin.pitch(X);
+        dbls[2] = pin.pitch(Y);
 
-    ints[0] = pin.num_regions();
-    ints[1] = pin.num_shells();
-    ints[2] = pin.region(0.0, 0.5401);
-    ints[3] = pin.region(0.0, 0.5399);
-    ints[4] = pin.matid(0);
-    ints[5] = pin.matid(1);
+        ints[0] = pin.num_regions();
+        ints[1] = pin.num_shells();
+        ints[2] = pin.region(0.0, 0.5401);
+        ints[3] = pin.region(0.0, 0.5399);
+        ints[4] = pin.matid(0);
+        ints[5] = pin.matid(1);
 
-    pin.get_extents(svs[0], svs[1]);
+        pin.get_extents(svs[0], svs[1]);
+    }
+
+    // Make the state and initialize
+    State state;
+    {
+        pin.initialize({0.0, 0.53, 0.0}, state);
+        ints[6] = state.region;
+        ints[7] = state.segment;
+        pin.initialize({0.0, 0.54, 0.0}, state);
+        ints[8]  = state.region;
+        ints[9]  = state.segment;
+        ints[10] = pin.cell(state.region, state.segment);
+        pin.initialize({0.0, 0.55, 0.0}, state);
+        ints[11] = state.region;
+        ints[12] = state.segment;
+        ints[13] = pin.cell(state.region, state.segment);
+    }
+
+    // Distance-to-boundary box-boundary tests
+    Vector r, omega;
+    {
+        r     = {0.0, 0.59, 0.0};
+        omega = {1.0, 0.0, 0.0};
+        pin.initialize(r, state);
+        pin.distance_to_boundary(r, omega, state);
+
+        dbls[3]  = state.dist_to_next_region;
+        ints[14] = state.exiting_face;
+        ints[15] = state.region;
+    }
+    {
+        r     = {0.0, 0.59, 0.0};
+        omega = {-1.0, 0.0, 0.0};
+        pin.initialize(r, state);
+        pin.distance_to_boundary(r, omega, state);
+
+        dbls[4]  = state.dist_to_next_region;
+        ints[16] = state.exiting_face;
+        ints[17] = state.region;
+    }
 }
 
 //---------------------------------------------------------------------------//
@@ -68,17 +110,17 @@ void Single_Shell::run()
     auto p1 = d1.device_instance();
     auto p2 = d2.device_instance();
 
-    thrust::device_vector<int>          ints(20, -1);
-    thrust::device_vector<double>       dbls(20, -1);
-    thrust::device_vector<Space_Vector> svs(20);
+    thrust::device_vector<int>    ints(20, -1);
+    thrust::device_vector<double> dbls(20, -1);
+    thrust::device_vector<Vector> svs(20);
 
     {
         single_shell_kernel1<<<1,1>>>(
             p1, ints.data().get(), dbls.data().get(), svs.data().get());
 
-        thrust::host_vector<int>          rints(ints.begin(), ints.end());
-        thrust::host_vector<double>       rdbls(dbls.begin(), dbls.end());
-        thrust::host_vector<Space_Vector> rsvs(svs.begin(), svs.end());
+        thrust::host_vector<int>    rints(ints.begin(), ints.end());
+        thrust::host_vector<double> rdbls(dbls.begin(), dbls.end());
+        thrust::host_vector<Vector> rsvs(svs.begin(), svs.end());
 
         EXPECT_EQ(0.54, dbls[0]);
         EXPECT_EQ(1.26, dbls[1]);
@@ -97,6 +139,23 @@ void Single_Shell::run()
         EXPECT_SOFTEQ(rsvs[1][X],  1.26/2, 1.e-12);
         EXPECT_SOFTEQ(rsvs[1][Y],  1.26/2, 1.e-12);
         EXPECT_SOFTEQ(rsvs[1][Z], 14.28  , 1.e-12);
+
+        EXPECT_EQ(0, rints[6]);
+        EXPECT_EQ(0, rints[7]);
+        EXPECT_EQ(0, rints[8]);
+        EXPECT_EQ(0, rints[9]);
+        EXPECT_EQ(0, rints[10]);
+        EXPECT_EQ(1, rints[11]);
+        EXPECT_EQ(0, rints[12]);
+        EXPECT_EQ(1, rints[13]);
+
+        EXPECT_SOFTEQ(rdbls[3], 0.63, 1.e-12);
+        EXPECT_EQ(State::PLUS_X, rints[14]);
+        EXPECT_EQ(1, rints[15]);
+
+        EXPECT_SOFTEQ(rdbls[4], 0.63, 1.e-12);
+        EXPECT_EQ(State::MINUS_X, rints[16]);
+        EXPECT_EQ(1, rints[17]);
     }
 
     {
